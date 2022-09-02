f7d9c33ebfcf04e2ad42f610df2c0f7fd956a2b7
ericjmachado <ericmachadoeng@gmail.com>
2022-09-02 19:57:12 -0300

    Subindo casos de teste no junit

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,19 +1,85 @@
-// Create a class called GameTest that tests the Game class.
-
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class GameTest {
 
+    Game game;
+
+    @Before
+    public void setup() {
+        game = new Game("hello");
+    }
+
+    @Test
+    public void test01() {
+        game.guess('h');
+        game.guess('e');
+        game.guess('l');
+        game.guess('o');
+        Assert.assertEquals(6, game.getRemainingTries());
+        Assert.assertTrue(game.isSolved());
+    }
+
+    @Test
+    public void test02() {
+        game.guess('o');
+        game.guess('b');
+        game.guess('d');
+        game.guess('g');
+        game.guess('y');
+        game.guess('t');
+        game.guess('r');
+        Assert.assertEquals(0, game.getRemainingTries());
+        Assert.assertFalse(game.isSolved());
+    }
+
+    @Test
+    public void test03() {
+        boolean result = game.guess('w');
+        Assert.assertFalse(result);
+    }
+
+
+    @Test
+    public void test04() {
+        boolean result = game.guess('h');
+        Assert.assertTrue(result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void test05() {
+        game.guess(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test06() {
+        game.guess("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test07() {
+        game.guess('2');
+    }
+
+    @Test
+    public void test08() {
+        boolean resultado = game.guess("h");
+        Assert.assertTrue(resultado);
+        Assert.assertEquals("h----", game.getCurrentProgress());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test09() {
+        boolean resultado = game.guess("h");
+        Assert.assertTrue(resultado);
+        game.guess("h");
+    }
+
     @Test()
-    public void testGameHello() {
-        Game g = new Game("hello");
-        g.guess('h');
-        g.guess('e');
-        g.guess('l');
-        g.guess('o');
-        Assert.assertEquals(6, g.getRemainingTries());
-        Assert.assertTrue(g.isSolved());
+    public void test10() {
+        String resultado = game.getAnswer();
+        Assert.assertEquals("hello", resultado);
     }
 }
